5d42e1c707108370291d390f2b4cf77eb734182c
Ernie Bello <ernie@ern.me>
2014-05-12 02:15:42 -0400

    added primary navigation functionality

--- plugins/contentHelpers.js
+++ plugins/contentHelpers.js
@@ -1,13 +1,11 @@
 module.exports = function(env, callback) {
   env.locals.urlbyid = function(contents, id) {
+    var pages = env.helpers.getAllPages(contents);
     var url;
-    for (var key in contents) {
-      var currentObj = contents[key];
-      if (isFile(currentObj)) {
-        if (currentObj.metadata.id == id) {
-          url = currentObj.url;
-          break;
-        }
+    for (var x = 0; x < pages.length; x++) {
+      if (pages[x].metadata.id == id) {
+        url = pages[x].url;
+        break;
       }
     }
     if (!url) {
@@ -16,9 +14,50 @@ module.exports = function(env, callback) {
     return url;
   }
 
+  env.helpers.getAllPages = function(contents) {
+    var output = env.locals.pages || [];
+    if (!env.locals.pages) {
+      for (var key in contents) {
+        var currentObj = contents[key];
+        if (isFile(currentObj)) {
+          // console.log('pushing page');
+          if(currentObj.metadata.hasOwnProperty("primarynavorder")) {
+            currentObj.primarynavorder = currentObj.metadata.primarynavorder;
+          }
+          output.push(currentObj);
+        }
+        else if(isDirectory(currentObj)) {
+          output = output.concat(env.helpers.getAllPages(currentObj));
+        }
+      }
+      env.locals.pages = output;
+    }
+    return output;
+	};
+
+  env.helpers.getPrimaryNavigation = function(contents) {
+    var pages = env.helpers.getAllPages(contents);
+    var primary_nav = pages.filter(function(page) {
+      return page.metadata.primarynavorder;
+    });
+    sortByKey(primary_nav, 'primarynavorder');
+    return primary_nav;
+	};
+
   callback();
 
   var isFile = function(currentItem) {
 		return currentItem.hasOwnProperty('__filename');
 	};
+
+  function sortByKey(arr, key) {
+    return arr.sort(function(a, b) {
+      var x = a[key], y = b[key];
+      // console.log(a['metadata.primarynavorder']);
+      // console.log(b.metadata.primarynavorder);
+      // console.log(x);
+      // console.log(y);
+      return (x < y) ? -1 : ((x > y) ? 1 : 0);
+    });
+  }
 };
